04cd4c258cd4eb725b07af787b21583a296d89a4
Qian Zhang <q.zhang@grs-sim.de>
2019-11-19 15:41:01 +0100

    Debug zhang's code

--- printNumbers/functions/fibonacci.py
+++ printNumbers/functions/fibonacci.py
@@ -39,10 +39,9 @@ def FibonacciSequence(n):
     # return (sequence)
     
     # Zhang's version
-    print('Zhang says,')
     fib = [0]*n
-    if n>=0: fib[0] = 0
-    if n>=1: fib[1] = 1
+    if n>0: fib[0] = 0
+    if n>1: fib[1] = 1
     for i in range(2,n):
         fib[i] = fib[i-1] + fib[i-2]
     return fib
